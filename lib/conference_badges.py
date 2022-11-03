6ad24d8f8b56b1f08e776bb0431be1ceb17dd267
Emiley Palmquist <54187547+emileypalmquist@users.noreply.github.com>
2022-11-03 09:27:05 -0700

    test passing

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,22 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
+
 
 def batch_badge_creator(names):
-    return None
+    return [badge_maker(name) for name in names]
+
+
+def room_maker(name, room):
+    return f"Hello, {name}! You'll be assigned to room {room}!"
+
 
 def assign_rooms(names):
-    return None
+    return [room_maker(name, index + 1) for index, name in enumerate(names)]
+
 
 def printer(names):
-    return None
+    for badge in batch_badge_creator(names):
+        print(badge)
+
+    for room_assignment in assign_rooms(names):
+        print(room_assignment)
